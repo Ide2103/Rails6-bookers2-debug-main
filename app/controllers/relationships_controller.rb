794ddb61ac0fa687ffd0ff5c2e8aa7ecb2a1a10a
Ide2103 <107347502+Ide2103@users.noreply.github.com>
2022-08-05 07:06:30 +0000

    chapter4-1

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,14 +1,26 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
   
-  def follow
-    current_user.follow(params[:id])
-    render ("users/follow")
-  end 
-    
-  def unfollow
-    current_user.unfollow(params[:id])
-    render ("users/unfollow")
-  end 
+  def create
+    user = User.find(params[:user_id])
+    current_user.follow(user)
+		redirect_to request.referer
+  end
+  
+  def destroy
+    user = User.find(params[:user_id])
+    current_user.unfollow(user)
+		redirect_to request.referer
+  end
+  
+  def followings
+    user = User.find(params[:user_id])
+		@users = user.followings
+  end
 
+  def followers
+    user = User.find(params[:user_id])
+		@users = user.followers
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,18 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about" => "homes#about", as: "about"
   
-  post "follow/:id" => "relationships#follow", as: "follow"
-  post "unfollow/:id" => "relationships#unfollow", as: "unfollow"
-  get "follow/:id" => "relationships#follow", as: "following_user"
-  get "unfollow/:id" => "relationships#unfollow", as: "followwer_user"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
    resources :book_comments, only: [:create, :destroy]
    resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+  	get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
+  end 
+
+  get "/search", to: "searches#search"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
